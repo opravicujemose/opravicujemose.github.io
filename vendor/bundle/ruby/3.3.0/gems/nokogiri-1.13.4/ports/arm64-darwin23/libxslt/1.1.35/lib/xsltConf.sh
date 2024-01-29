cb74eb7a169d488c5b4c39f380f77be30a0e337c
Davorin Pavlica <davorin@zvpl.com>
2024-01-29 19:15:41 +0100

    192

--- vendor/bundle/ruby/3.3.0/gems/nokogiri-1.13.4/ports/arm64-darwin23/libxslt/1.1.35/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/dpavlica/Documents/GitHub/opravicujemose.github.io/vendor/bundle/ruby/3.3.0/gems/nokogiri-1.13.4/ports/arm64-darwin23/libxslt/1.1.35/lib"
-XSLT_LIBS="-lxslt -L/Users/dpavlica/Documents/GitHub/opravicujemose.github.io/vendor/bundle/ruby/3.3.0/gems/nokogiri-1.13.4/ports/arm64-darwin23/libxml2/2.9.13/lib -lxml2 -lz -L/opt/homebrew/Cellar/xz/5.4.5/lib -llzma -lpthread -liconv -lm "
-XSLT_PRIVATE_LIBS=""
-XSLT_INCLUDEDIR="-I/Users/dpavlica/Documents/GitHub/opravicujemose.github.io/vendor/bundle/ruby/3.3.0/gems/nokogiri-1.13.4/ports/arm64-darwin23/libxslt/1.1.35/include"
-MODULE_VERSION="xslt-1.1.35"
